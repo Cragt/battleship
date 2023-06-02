faff1e8a22053820efa585dd8f97b5e4068130ac
Craig <cragt369@gmail.com>
2023-06-01 20:18:39 -0500

    Create player factory, create gameLoop function, add New Game button and event listener

--- src/index.js
+++ src/index.js
@@ -89,6 +89,66 @@ function createGameBoard() {
     }
   }
 
+  function player2Board() {
+    const cellWidth = 64;
+    const cellHeight = 64;
+
+    const cells = document.createElement("canvas");
+    cells.width = board[0].length * cellWidth;
+    cells.height = board.length * cellHeight;
+    document.body.appendChild(cells);
+    const context = cells.getContext("2d");
+
+    for (let i = 0; i < board.length; i++) {
+      for (let a = 0; a < board[0].length; a++) {
+        if (i % 2 == 0) {
+          if (a % 2 == 0) {
+            context.fillStyle = "#aaaaaa";
+          } else {
+            context.fillStyle = "#888888";
+          }
+        } else {
+          if (a % 2 == 1) {
+            context.fillStyle = "#aaaaaa";
+          } else {
+            context.fillStyle = "#888888";
+          }
+        }
+        context.fillRect(a * cellWidth, i * cellHeight, cellWidth, cellHeight);
+        switch (board[i][a]) {
+          case "W":
+            context.fillStyle = "#ffffff";
+            context.beginPath();
+            context.arc(
+              a * cellWidth + cellHeight / 2,
+              i * cellHeight + cellHeight / 2,
+              10,
+              0,
+              Math.PI * 2,
+              true
+            );
+            context.closePath();
+            context.fill();
+            break;
+          case "B":
+            context.fillStyle = "#000000";
+            context.beginPath();
+            context.arc(
+              (a * cellWidth) / 2,
+              i * cellHeight + cellHeight / 2,
+              10,
+              0,
+              Math.PI * 2,
+              true
+            );
+            context.closePath();
+            context.fill();
+            break;
+        }
+      }
+    }
+  }
+
   function placeShip(row, col, ship) {
     space = 0;
     if (row < 0 || row >= size || col < 0 || col >= size) {
@@ -132,6 +192,7 @@ function createGameBoard() {
 
   return {
     player1Board,
+    player2Board,
     placeShip,
     receiveAttack,
     getCellStatus,
@@ -145,23 +206,31 @@ function player(name, turn, isComputer) {
     isComputer: isComputer,
   };
 }
+const gameBoard = createGameBoard();
 
 function gameLoop() {
   let newGameBtn = document.createElement("button");
   newGameBtn.innerText = "New Game";
+
   gameArea.appendChild(newGameBtn);
+  newGameBtn.addEventListener("click", (event) => {
+    newGameBtn.style.display = 'none';
+    let player1Name = prompt("Enter name for player 1");
+    let player2Name = prompt("Enter name for player 2");
+    gameBoard.player1Board();
+    gameBoard.player2Board();
+  })
 }
-// gameLoop();
+gameLoop();
 let player1 = player("Craig", 0, 0);
 // console.log(player1);
-const gameBoard = createGameBoard();
 let destroyer = ship("destroyer", 2, 0, false);
 let submarine = ship("submarine", 3, 0, false);
 let cruiser = ship("cruiser", 3, 0, false);
 let battleship = ship("battleship", 4, 0, false);
 let carrier = ship("carrier", 5, 0, false);
 gameBoard.placeShip(1, 1, destroyer);
-gameBoard.player1Board();
+// gameBoard.player1Board();
 gameBoard.receiveAttack(1, 1, destroyer);
 console.log(destroyer);
 gameBoard.receiveAttack(2, 1, destroyer);
